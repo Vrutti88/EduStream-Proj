5eaee2538607c4fde97291a48fdf98c23fed23ec
Vrutti Patil <vruttipatil1396@gmail.com>
2026-06-15 21:57:37 +0530

    Update rds_s3_cloudwatch.tf

--- terraform/rds_s3_cloudwatch.tf
+++ terraform/rds_s3_cloudwatch.tf
@@ -35,7 +35,7 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name    = aws_db_subnet_group.main.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   multi_az                = false
-  backup_retention_period = 7
+  backup_retention_period = 1
   skip_final_snapshot     = true
   final_snapshot_identifier = "${var.project_name}-final-snapshot"
 
