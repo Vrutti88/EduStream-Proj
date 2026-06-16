a10a98920b820adfe62861903f993d4bf876194f
Vrutti Patil <vruttipatil1396@gmail.com>
2026-06-16 10:35:12 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -66,9 +66,9 @@ resource "aws_eks_node_group" "main" {
   instance_types  = [var.node_instance_type]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 4
-    min_size     = 2
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
   }
 
   depends_on = [
